38f7055cb19c69c1b7398621d4a7aeea2b54f9b2
EricMensah <eric.m4511@gmail.com>
2017-10-10 14:28:04 +0000

    Create helloworld.cpp
    hello world  file creation.

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+// this code design by Eric Mensah 
+// a member of teamkeenload. 
+/*********************************/
+
+#include <iostream>
+
+int main ()
+{
+std::cout << "hello world! " << std::endl; 
+}
